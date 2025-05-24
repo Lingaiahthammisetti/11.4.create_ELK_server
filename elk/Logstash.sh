6a63200502df80a8a8279aebcd1b4393c89cf037
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-05-24 12:55:56 +0530

    ELK server

--- elk/Logstash.sh
+++ elk/Logstash.sh
@@ -27,19 +27,32 @@ else
     echo "You are super user."
 fi
 
-sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch &>>$LOGFILE
-VALIDATE $? "elasticsearch GPG-KEY"
+# sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch &>>$LOGFILE
+# VALIDATE $? "elasticsearch GPG-KEY"
+
+# echo "
+# [logstash-8.x]
+# name=Elastic repository for 8.x packages
+# baseurl=https://artifacts.elastic.co/packages/8.x/yum
+# gpgcheck=1
+# gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+# enabled=1
+# autorefresh=1
+# type=rpm-md
+#  " > /etc/yum.repos.d/logstash.repo
+
 
 echo "
-[logstash-8.x]
-name=Elastic repository for 8.x packages
-baseurl=https://artifacts.elastic.co/packages/8.x/yum
+[logstash-9.x]
+name=Elastic repository for 9.x packages
+baseurl=https://artifacts.elastic.co/packages/9.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
 autorefresh=1
 type=rpm-md
  " > /etc/yum.repos.d/logstash.repo
+
 
 yum install logstash -y &>>$LOGFILE
 VALIDATE $? "logstash Installation"
