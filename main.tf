aa979c978d41056b158855c6d055017b103954d5
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-18 00:23:15 +0530

    create ELK server

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "elk_ec2" {
     ami           = data.aws_ami.rhel_info.id
     instance_type = var.ec2_instance.instance_type
     vpc_security_group_ids = [var.allow_everything]
+
+    root_block_device {
+        encrypted             = false
+        volume_type           = "gp3"
+        volume_size           = 50
+        iops                  = 3000
+        throughput            = 125
+        delete_on_termination = true
+    }
     tags = {
         Name = "ELK_server"
     }
